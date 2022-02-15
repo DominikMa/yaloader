f1c0a47ee2c7b23ad0bf625fd38e7d24c1ac7afb
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2022-02-15 18:25:22 +0100

    Add tests for loads decorator

---
+++ tests/test_loads_decorator.py
@@ -0,0 +1,32 @@
+import dataclasses
+
+import yaloader
+from yaloader import YAMLBaseConfig
+
+
+def test_loads_constructor(yaml_loader, config_loader):
+    @dataclasses.dataclass
+    class Class:
+        attribute: int
+
+    @yaloader.loads(Class)
+    class ClassConfig(YAMLBaseConfig, yaml_loader=yaml_loader):
+        attribute: int = 0
+
+    config = config_loader.construct_from_string("!Class {attribute: 1}")
+    klass = config.load()
+    assert isinstance(klass, Class)
+
+
+def test_loads_constructor_auto_load(yaml_loader, config_loader):
+    @dataclasses.dataclass
+    class Class:
+        attribute: int
+
+    @yaloader.loads(Class)
+    class ClassConfig(YAMLBaseConfig, yaml_loader=yaml_loader):
+        attribute: int = 0
+
+    klass = config_loader.construct_from_string("!Class {attribute: 1}", auto_load=True)
+    assert isinstance(klass, Class)
+
